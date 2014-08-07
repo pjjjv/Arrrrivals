2c5c99165834c77f7f90e1066c362822408f91ed
pjv <ezelspinguin@gmail.com>
2014-08-08 01:05:45 +0200

    json collected

--- web/ar-board.dart
+++ web/ar-board.dart
@@ -2,6 +2,14 @@ import 'package:polymer/polymer.dart';
 
 @CustomTag('ar-board')
 class Board extends PolymerElement {
+  @published var arrivals2;
+        
+  get arrivals => arrivals2;
+  
+  void set arrivals(var arrivals3) {
+      arrivals2 = arrivals3;
+  }
+        
   Board.created() : super.created() {
     print("board created");
   }
