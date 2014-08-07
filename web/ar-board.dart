f70af3b06799c8e1c662ebdc56e46b9f532c9cb1
pjv <ezelspinguin@gmail.com>
2014-08-08 00:41:17 +0200

    Stuck on on

---
+++ web/ar-board.dart
@@ -0,0 +1,8 @@
+import 'package:polymer/polymer.dart';
+
+@CustomTag('ar-board')
+class Board extends PolymerElement {
+  Board.created() : super.created() {
+    print("board created");
+  }
+}
---
+++ web/arrrrivals.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+
+
+
+
+arrivalsLoaded(e, detail, node) {
+  print("arrivalsLoaded4");
+}
+
+void main() {
+  /*initPolymer().run(() {
+    print("main1");
+  });*/
+}
