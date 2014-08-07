631b286aa5bfe061f67bc76f9b5b5a8556e89847
pjv <ezelspinguin@gmail.com>
2014-08-08 01:31:08 +0200

    board gets arrivals but not showing

--- web/ar-board.dart
+++ web/ar-board.dart
@@ -1,13 +1,21 @@
 import 'package:polymer/polymer.dart';
+import 'dart:collection';
 
 @CustomTag('ar-board')
 class Board extends PolymerElement {
-  @published var arrivals2;
+  @published LinkedHashMap arrivals2;
         
-  get arrivals => arrivals2;
+  get arrivalses { 
+    print('board get arrivalses: '+arrivals2.toString());  
+    return arrivals2;
+  }
   
-  void set arrivals(var arrivals3) {
-      arrivals2 = arrivals3;
+  void set arrivalses(var arrivals3) {
+    arrivals2 = arrivals3;
+    print('board set arrivalses: '+arrivals2.toString()); 
+    if (arrivals2 != null) {
+      print('      set arrivalses1: '+arrivals2['time'].toString());  
+    } 
   }
         
   Board.created() : super.created() {
