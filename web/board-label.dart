0a8538a8e417bb98414d1c3e7a369182402dbc47
pjv <ezelspinguin@gmail.com>
2014-08-07 23:16:49 +0200

    Trimmed down tryoing to circumvent dart bugs

---
+++ web/board-label.dart
@@ -0,0 +1,13 @@
+import 'package:polymer/polymer.dart';
+
+@CustomTag('board-label')
+class BoardLabel extends PolymerElement {
+  BoardLabel.created() : super.created() {
+    print("board label created");
+  }
+}
+
+/*main() {
+  initPolymer().run(() {
+  });
+}*/
